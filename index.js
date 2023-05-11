3978e3a1f61606a881e79989b2f9572622c4bc8a
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-05-11 17:15:41 -0400

    dom_modifying_lab_complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "VIK is the champion";
+
+
+
